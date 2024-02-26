a3ba590c6fc48d4cb6abf91776735e35aa9957fc
Satish Y <satishjs2297@gmail.com>
2024-02-26 06:51:23 +0530

    add providers

--- ACR/main.tf
+++ ACR/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.5.7"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.43.0"
-    }
-  }
   backend "azurerm" {
     resource_group_name  = "billing-review-sa-rg"
     storage_account_name = "billingreviewsa"
